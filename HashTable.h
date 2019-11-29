b53460fb4b2cca5fbd6589ea8a37dc99a434f438
rabihelheloutamu <58325521+rabihelheloutamu@users.noreply.github.com>
2019-11-29 02:35:50 -0600

    Add files via upload

---
+++ HashTable.h
@@ -0,0 +1,27 @@
+#ifndef HASH_H
+#define HASH_H
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+
+class HashTable{				//Universal Hash Table Definition
+protected:
+	int mod;					//remainder of what? = mod
+	int hash(string s);			//returns the position key s needs to be placed in hash table
+
+public: 
+	HashTable();				//constructor
+	~HashTable();				//destructor
+
+	virtual void insert(string key) = 0;					//function to insert a string key into a known position in the hash table
+	virtual int remove(string key) = 0;						//function to remove key string from hash table
+	virtual int get(string key) = 0;						//get information (# of repetitions) of certain key
+	virtual void printAll(string filename) = 0;				//print all unique words with info next to them
+	virtual bool isEmpty() const = 0;						//is hash empty? -- where hash is represented by the protected vector
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "HashTable.h"
+
+int main()
+{
+	/**vector<string> vec;						//vector to represent the raw txt file
+	string str;								//string variable to fill each position in vector
+	ifstream intxt("dictionary.txt");		//initialize the ifstream operator
+	
+	while (getline(intxt, str)) {			//while getting the string of each line and saving it as str
+		vec.push_back(str);					//push the str to the back of the vector
+	}**/
+
+}
