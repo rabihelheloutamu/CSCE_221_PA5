391964f8dd3ef3ce42659714bb4ada3ec593b806
rabihelheloutamu <rabih_el_helou@tamu.edu>
2019-11-29 14:43:02 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	/**vector<string> vec;						//vector to represent the raw txt file test
+	/**vector<string> vec;					//vector to represent the raw txt file
 	string str;								//string variable to fill each position in vector
 	ifstream intxt("dictionary.txt");		//initialize the ifstream operator
 	
