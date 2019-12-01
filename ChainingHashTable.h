d440bd1499f6c6e33295ffca50d9c32ef4df01a1
rabihelheloutamu <rabih_el_helou@tamu.edu>
2019-12-01 14:17:32 -0600

    Update ChainingHashTable.h
    initializing the Chaining Hash Table as a vector of buckets, where the buckets are vectors of strings.

--- ChainingHashTable.h
+++ ChainingHashTable.h
@@ -3,17 +3,15 @@
 
 #include "HashTable.h"
 
-
-// Chaining hash table class
-class ChainingHashTable: public HashTable {
-    private:
-    // TODO: insert additional variables needed here
-
-    public: 
-    ChainingHashTable();
-    ~ChainingHashTable(); 
-    void insert(std::string key, int val); 
-    int remove(std::string key); 
+class ChainingHashTable: public HashTable {			//chaining hash table class
+private:
+	vector< vector<string> > vec;					/**Hash Table:  vector of buckets that stack strings
+													   Bucket    :  a vector of strings using vector stl	**/
+public:
+	ChainingHashTable();
+    ~ChainingHashTable();
+    void insert(std::string key, int val);
+    int remove(std::string key);
     int get(std::string key);
     void printAll(std::string filename);
 };
