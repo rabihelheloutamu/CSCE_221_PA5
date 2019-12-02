dd147f92c722dfcbf2fc7f6ab4c9ed772ef6291c
rabihelheloutamu <rabih_el_helou@tamu.edu>
2019-12-02 14:13:48 -0600

    Update ChainingHashTable.h
    Switched Key Class to hash table because it will be used in all hash methods

--- ChainingHashTable.h
+++ ChainingHashTable.h
@@ -3,19 +3,7 @@
 
 #include "HashTable.h"
 
-class Key {		//defenition of a key for our purpose in chaining hash table:
-private:
-	string word;	//1) a string of characters aka a word
-	int rep;		//2) how many times it was repeated
-public:
-	Key();
-	~Key();
 
-	void setWord(string str);			//function to set the string of a key
-	void setRep(int val);
-	string getWord();
-	int getRep();
-};
 
 class ChainingHashTable: public HashTable {			//chaining hash table class
 private:
