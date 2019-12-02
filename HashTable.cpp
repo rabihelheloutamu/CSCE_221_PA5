727f1bb06b857b658bafb2381ab7a13d765d507f
rabihelheloutamu <rabih_el_helou@tamu.edu>
2019-12-02 14:13:18 -0600

    Update HashTable.cpp
    Switched the Key Class functions to HashTable .cpp because it will be used for all Hash Types

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,6 +1,34 @@
 #include "HashTable.h"
 
+Key::Key() {
+	rep = 1;		//each key alone has one repetition untill added to another key
+}
+
+Key::~Key() {
+}
+
+void Key::setWord(string str) {
+	word = str;
+}
+
+void Key::setRep(int val) {
+	rep = val;
+}
+
+string Key::getWord() {
+	return word;
+}
+
+int Key::getRep() {
+	return rep;
+}
+
 HashTable::HashTable() {  //default constructor
+	mod = 58000;								//to reduce collisions, mod was chosen to be the number of unique words
+
+	cst = 181;									/**taking "cab" to be the smallest ascii valued 3 letter word, 181 is the
+												   smallest integer we can multiply such that every final ascii value is
+												   is greater than 53000, so that ascii % mod is a valid return number   **/
 }
 
 HashTable::~HashTable() { //default destructor
