bde5afee71ebf4b9fc06d9d2354e2309ad9cde93
rabihelheloutamu <rabih_el_helou@tamu.edu>
2019-12-02 15:51:34 -0600

    Update ProbingHashTable.cpp
    Defining the Insert for the linear probing

--- ProbingHashTable.cpp
+++ ProbingHashTable.cpp
@@ -1,33 +1,48 @@
 #include "ProbingHashTable.h"
 
-// constructor (NOTE: graders will use a default constructor for testing)
+
 ProbingHashTable::ProbingHashTable() {
-	
+	vector<Key> v(116009);						//116009 will be the number of Keys to reduce the number of collisions
+	vec = v;									//set the private vector to a vector of buckets of size 116009
 }
 
-// destructor
-ProbingHashTable::~ProbingHashTable() {
+
+ProbingHashTable::~ProbingHashTable() {			//vector already has built in destructors
 
 }
 
-// inserts the given string key
-void ProbingHashTable::insert(std::string key, int val) {
 
+void ProbingHashTable::insert(string key) {
+	int h = hash(key);
+
+	while (vec[h].getWord().size() != 0) {
+		if (vec[h].getWord() == key) {
+			vec[h].setRep(vec[h].getRep() + 1);
+			return;
+		}
+		else {
+			h++;
+		}
+	}
+
+	Key k;
+	k.setWord(key);
+	vec[h] = k;
 }
 
 // removes the given key from the hash table - if the key is not in the list, throw an error
-int ProbingHashTable::remove(std::string key) {
+int ProbingHashTable::remove(string key) {
 
 	return 0;
 }
 
 // getter to obtain the value associated with the given key
-int ProbingHashTable::get(std::string key) {
+int ProbingHashTable::get(string key) {
 
 	return 0;
 }
 
 // prints number of occurrances for all given strings to a txt file
-void ProbingHashTable::printAll(std::string filename) {
+void ProbingHashTable::printAll(string filename) {
 
 }
