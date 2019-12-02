e219f2cea09bcdfc39ac3e9ad4bdeeb1abee0bde
rabihelheloutamu <rabih_el_helou@tamu.edu>
2019-12-02 14:38:24 -0600

    Update main.cpp
    deleted the useless comments

--- main.cpp
+++ main.cpp
@@ -2,12 +2,5 @@
 
 int main()
 {
-	/**vector<string> vec;					//vector to represent the raw txt file
-	string str;								//string variable to fill each position in vector
-	ifstream intxt("dictionary.txt");		//initialize the ifstream operator
 	
-	while (getline(intxt, str)) {			//while getting the string of each line and saving it as str
-		vec.push_back(str);					//push the str to the back of the vector
-	}**/
-
 }
