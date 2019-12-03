4deb2bed50dcfa09b2f4fb629a32c502032cc469
rabihelheloutamu <rabih_el_helou@tamu.edu>
2019-12-03 00:43:42 -0600

    Updates

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "ProbingHashTable.h"
 #include "DoubleHashTable.h"
 #include "ChainingHashTable.h"
+#include "StopWatch.h"
 
 int main()
 {
@@ -11,4 +12,14 @@ int main()
 	//P.printAll("dictionary.txt");
 	//D.printAll("dictionary.txt");
 	//C.printAll("dictionary.txt");
+	/*
+	t.start();
+	for (int i = 0; i < 3000; i++) {
+		if (i % 100 == 0) {				//every multiple of 100
+			cout << t.ms() << endl;		//output the time taken since we started
+		}
+		U.insertItem(1);				//we insert 100 before the if statement is tested
+	}
+	t.stop();
+	*/
 }
