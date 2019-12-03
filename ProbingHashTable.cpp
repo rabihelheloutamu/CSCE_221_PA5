45f2f5c19299112024454aba2c2d1f623d340802
rabihelheloutamu <rabih_el_helou@tamu.edu>
2019-12-02 18:53:59 -0600

    Update ProbingHashTable.cpp
    Added remove, get, and printAll

--- ProbingHashTable.cpp
+++ ProbingHashTable.cpp
@@ -13,36 +13,62 @@ ProbingHashTable::~ProbingHashTable() {			//vector already has built in destruct
 
 
 void ProbingHashTable::insert(string key) {
-	int h = hash(key);
+	int h = hash(key);									//find the initial position of the word
 
-	while (vec[h].getWord().size() != 0) {
-		if (vec[h].getWord() == key) {
-			vec[h].setRep(vec[h].getRep() + 1);
-			return;
+	while (vec[h].getWord().size() != 0) {				//while the position we are on is taken
+		if (vec[h].getWord() == key) {					//if that position already contains the word
+			vec[h].setRep(vec[h].getRep() + 1);			//then just add 1 to the value
+			return;										//and return
 		}
-		else {
-			h++;
+		else {											//if not, then that means a collision occured
+			h++;										//on to the next hash position (hash +1)
 		}
 	}
-
-	Key k;
-	k.setWord(key);
-	vec[h] = k;
+														//for loop exited that means the position is empty
+	Key k;												//initialize a key
+	k.setWord(key);										//with the input word
+	vec[h] = k;											//the vector with set position is now the intended key
 }
 
-// removes the given key from the hash table - if the key is not in the list, throw an error
-int ProbingHashTable::remove(string key) {
 
+int ProbingHashTable::remove(string key) {
+	Key k;																		//initialize a key
+	k.setRep(0);																//make that key completely emtpy
+	
+	for (int i = 0; i < vec.size(); i++) {										//iterate through the entire vector since collisions may have occured
+		if (vec[i].getWord() == key) {											//if the word is found
+			int out = vec[i].getRep();											//save it's value for outputting
+			vec[i] = k;															//set the value to empty
+			return out;															//return the old value
+		}
+	}
+	
+	throw std::runtime_error("Remove Error: Key not found in Hash Table");		//for loop exited that means the key didn't exist
 	return 0;
 }
 
 // getter to obtain the value associated with the given key
 int ProbingHashTable::get(string key) {
+	for (int i = 0; i < vec.size(); i++) {										//iterate through the entire vector since collisions may have occured
+		if (vec[i].getWord() == key) {											//if the word is found
+			return vec[i].getRep();												//return it's value
+		}
+	}
 
+	throw std::runtime_error("Remove Error: Key not found in Hash Table");		//for loop exited that means the key didn't exist
 	return 0;
 }
 
-// prints number of occurrances for all given strings to a txt file
-void ProbingHashTable::printAll(string filename) {
 
+void ProbingHashTable::printAll(string filename) {
+	string str;								//string variable to fill each position in vector
+	ifstream intxt(filename);				//initialize the ifstream operator
+
+	while (getline(intxt, str)) {			//while getting the string of each line and saving it as str
+		insert(str);						//push the str to the back of the vector
+	}
+
+	for (int i = 0; i < vec.size(); i++) {										//for every key
+			cout << vec[i].getWord() << ": " << vec[i].getRep() << endl;		//output its word and give its value of repetition
+	}
 }
